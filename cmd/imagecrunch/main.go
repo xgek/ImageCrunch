bd1832952c0e120b268cd334c1190713d56b6cea
XGEK <73622401+xgek@users.noreply.github.com>
2025-08-03 21:36:12 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/imagecrunch/main.go
@@ -0,0 +1,23 @@
+// cmd/imagecrunch/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"imagecrunch/internal/imagecrunch"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := imagecrunch.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
